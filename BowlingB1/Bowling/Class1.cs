15f6fbfcba98d772a1161f5a326cde29c631c44a
AlmantasK <almantusk@gmail.com>
2023-02-14 20:56:55 +0200

    chore: initial preparation for kata

---
+++ BowlingB1/Bowling/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace Bowling
+{
+    public class Class1
+    {
+        public int Foo()
+        {
+            return 0;
+        }
+    }
+}
---
+++ EmptyKata/Bowling.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Bowling.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            new Class1().Foo().Should().Be(0);
+        }
+    }
+}
